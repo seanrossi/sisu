b2635f2eda23e4ef9037b510251f0d81763cb81e
jocelyntan11 <40223129+jocelyntan11@users.noreply.github.com>
2018-12-09 18:30:51 -0800

    Add files via upload

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('users/', views.HomePageView.as_view(), name='home'),
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
